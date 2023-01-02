f939e38446f68ed726db4a8f3319267fc59b6c81
ccosta-c <ccosta-c@student.42porto.com>
2023-01-02 11:41:47 +0000

    wrote function

--- ft_integer.c
+++ ft_integer.c
@@ -6,7 +6,32 @@
 /*   By: ccosta-c <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/16 11:59:06 by ccosta-c          #+#    #+#             */
-/*   Updated: 2022/12/16 11:59:09 by ccosta-c         ###   ########.fr       */
+/*   Updated: 2023/01/02 11:20:50 by ccosta-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libprintf.h"
+
+int		ft_integer(long nb)
+{
+	int	count;
+	char *base;
+
+	base = "0123456789";
+	count = 0;
+	if (nb < 0)
+	{
+		nb *= -1; 
+		count += ft_char('-');
+	}
+	if (nb >= 10)
+	{
+		count += ft_integer(nb / 10);
+		count += ft_integer(nb % 10);
+	}
+	if (nb < 10)
+	{
+		count += ft_char(*(base + nb));
+	}
+	return (count);
+}
