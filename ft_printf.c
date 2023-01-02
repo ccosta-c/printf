5bc924159feab3d0fc8b4be4f11b3f6af7e71dd3
ccosta-c <ccosta-c@student.42porto.com>
2023-01-02 16:21:32 +0000

    uploud

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: ccosta-c <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/12 14:26:29 by ccosta-c          #+#    #+#             */
-/*   Updated: 2023/01/02 11:27:05 by ccosta-c         ###   ########.fr       */
+/*   Updated: 2023/01/02 16:19:03 by ccosta-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,8 @@ static size_t	check_conversion(const char *str, va_list var_args)
 	{
 		count += ft_integer(va_arg(var_args, int));
 	}
+	if (*(str + 1) == 'x' || *(str + 1) == 'X')
+		count += ft_hex(va_arg(var_args, unsigned long), *(str + 1));
 	return (count);
 }
 
@@ -47,8 +49,11 @@ int	ft_printf(const char *str, ...)
 	return (count);
 }
 
+/*
 int main(void)
 {
-	printf("\n%d\n", ft_printf("%d", -2147483648));
+	ft_printf("%X", 0xFFFFFFFF);
+	printf("\n%x\n", 0xFFFFFFFF);
 	return (0);
 }
+*/
