dc44a0fd9b02a929a0008b1e95d96a82c887d177
ccosta-c <ccosta-c@student.42porto.com>
2023-01-05 14:05:28 +0000

    changes

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: ccosta-c <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/12 14:26:29 by ccosta-c          #+#    #+#             */
-/*   Updated: 2023/01/04 17:53:20 by ccosta-c         ###   ########.fr       */
+/*   Updated: 2023/01/05 14:00:40 by ccosta-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,20 +19,20 @@ static size_t	check_conversion(const char *str, va_list var_args)
 	count = 0;
 	if (*(str + 1) == '%')
 	{
-		count += write(1, "%", 1);
+		count += ft_char('%');
 	}
 	if (*(str + 1) == 'i' || *(str + 1) == 'd')
-	{
 		count += ft_integer(va_arg(var_args, int));
-	}
 	if (*(str + 1) == 'x' || *(str + 1) == 'X')
-		count += ft_hex(va_arg(var_args, unsigned long), *(str + 1));
+		count += ft_hex(va_arg(var_args, unsigned int), *(str + 1));
 	if (*(str + 1) == 'c')
-		count += ft_char(va_arg(var_args, int));
+		count += ft_char(va_arg(var_args, unsigned int));
 	if (*(str + 1) == 's')
 		count += ft_string(va_arg(var_args, char*));
 	if (*(str + 1) == 'u')
 		count += ft_unsigned(va_arg(var_args, unsigned int));
+	if (*(str + 1) == 'p')
+		count += ft_adress(va_arg(var_args, unsigned long));
 	return (count);
 }
 
@@ -55,18 +55,19 @@ int	ft_printf(const char *str, ...)
 			count += ft_char(*str);
 		}
 		str++;
+		va_end(var_args);
 	}
 	return (count);
 }
  
-int main(void)
+/* int main(void)
 {
 	int	counter_mine;
 	int	counter_official;
 
 	counter_official = 0;
-	counter_mine = ft_printf("\n%s\n", "FODASSE");
-	counter_official = printf("\n%p\n", &counter_official);
+	counter_mine = ft_printf("\n%p\n", LONG_MAX);
+	counter_official = printf("\n%p\n", LONG_MAX);
 	printf("counter_mine%i\ncounter_official%i\n", counter_mine, counter_official);
 	return (0);
-}
+} */
